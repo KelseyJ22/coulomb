0b03a87e0881153ebd991ad73558ae92dcc3ceae
Kelsey Josund <kelsey2@stanford.edu>
2015-04-25 22:38:53 -0700

    untested draft: coulomb's law visualization script

---
+++ particles.py
@@ -0,0 +1,11 @@
+import simulate.py
+
+def create_particlea:
+	one = {'loc':[x,y], 'velocity':v, 'mass':m, 'charge':q}
+	two = {'loc':[x,y], 'velocity':v, 'mass':m, 'charge':q}
+	three = {'loc':[x,y], 'velocity':v, 'mass':m, 'charge':q}
+	return [one, two, three]
+
+
+particles = create_particles()
+simulate.py particles[0] particles[1] particles [2]
