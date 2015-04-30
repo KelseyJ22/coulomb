52a0ed6931e1d3f8825a148463ebc6ed397f6305
Kelsey Josund <kelsey2@stanford.edu>
2015-04-30 12:49:35 -0700

    found the bug, not sure how to fix it...

--- particles.py
+++ particles.py
@@ -30,49 +30,58 @@ class Particles():
 		force = (numerator/denominator)
 
 		if ((a['charge'] > 0.0) & (b['charge'] > 0.0)) | ((a['charge'] < 0.0) & (b['charge'] < 0.0)): # attractive or repulsive forces
-			force *= -1
+			force *= -1 # negative indicates repulsive forces
 		return force
 
 
 	# calculates the total force on a particle in either the x or y coordinate
-	def combine(self, f2, f3, curr_loc, loc2, loc3, coord):
+	def combine(self, f1, f2, curr_loc, loc1, loc2, coord):
 		curr = curr_loc[coord]
-		p1 = loc2[coord]
-		p2 = loc3[coord]
-		r1 = self.distance(curr_loc, loc2)
-		r2 = self.distance(curr_loc, loc3)
+		p1 = loc1[coord]
+		p2 = loc2[coord]
+		r1 = self.distance(curr_loc, loc1)
+		r2 = self.distance(curr_loc, loc2)
 
-		f1 = f2 * ((curr - p1)/r1) # f1cos(theta)
-		f2 = f3 * ((curr - p2)/r2) # f2cos(theta)
+		f1 = f1 * ((curr - p1)/r1) # f1cos(theta) --> opp/hyp
+		f2 = f2 * ((curr - p2)/r2) # f2cos(theta) --> opp/hyp
 
 		f_tot = f1 + f2
+		
+		# THE PROBLEM IS:
+		# negative in force indicates repulsive, but for Fba, a[x] > b[x] it should be POSITIVE
+		# while for Fab, a[x] > b[x] it should be negative (pushing the particles apart)
+		# NEED TO FIGURE THIS OUT!!!
+
 		return f_tot # pythagorean theorem
 
 
-	# calculate vfinal from given information
+	# calculate vfinal from given information -- x or y direction
 	def calc_vfinal(self, vi, a, t):	
 		vf = (math.pow(vi, 2) + (2*a*t))
 		if vf < 0: # guard against negative velocity -- can be negative, but can't calculate this way
 			vf *= -1
 			vf = math.sqrt(vf)
-			vf *= -1
+			vf *= -1 # needs to end up negative (repulsive)
 			return vf # ewww this style is gross but whatever
 		else:
-			return math.sqrt((math.pow(vi, 2) + (2*a*t))) # kinematics
+			return math.sqrt(vf) # kinematics
 
 
-	# determine acceleration from given information
+	# determine acceleration in x or y direction
 	# (simulate based on constant-acceleration even though that's not what really happens)
 	def calc_acceleration(self, index, coord):
 		curr = self.particles[index]
 		f1 = self.forces[index]
-		if index + 1 < len(self.particles):
+		if (index + 1) < len(self.particles):
 			loc1 = self.particles[index + 1]['loc']
 		else:
-			loc1 = self.particles[0]['loc']
+			loc1 = self.particles[0]['loc'] # roll over to initial force
 		f2 = self.forces[index - 1]
-		loc2 = self.particles[index - 1]['loc']
+		loc2 = self.particles[index - 1]['loc'] # python automatically wraps around
+
+		# total force in x or y direction
 		f_tot = self.combine(f1, f2, curr['loc'], loc1, loc2, coord)
+
 		a = f_tot/curr['mass']
 		return a
 
@@ -82,17 +91,16 @@ class Particles():
 		p = self.particles[index]
 		a = self.calc_acceleration(index, coord)
 
-		# x = x + vt + 1/2at^2
+		# xf = xi + vt + 1/2at^2
 		if coord == 0:
 			final = p['loc'][coord] + p['vx'] + (1/2)*a*(math.pow(t, 2))
-			p['vx'] = self.calc_vfinal(p['vx'], a, t)
+			p['vx'] = self.calc_vfinal(p['vx'], a, t) # update for next iteration
 
 		else:
 			final = p['loc'][coord] + p['vy'] + (1/2)*a*(math.pow(t, 2))
-			p['vy'] = self.calc_vfinal(p['vy'], a, t)
+			p['vy'] = self.calc_vfinal(p['vy'], a, t) # update for next iteration
 
-		# update vx and loc for next iteration
-		p['loc'][coord] = final
+		p['loc'][coord] = final # update loc for next iteration
 		return final
 
 
@@ -113,20 +121,17 @@ class Particles():
 			locations = []
 			locations.append(particle['loc'][coord])
 			for increment in range(1, 100): # incremental progression through the time period
-				if coord == 0:
-					position = self.calc_pos_final(t/increment, count, coord)
-				else:
-					position = self.calc_pos_final(t/increment, count, coord)
+				position = self.calc_pos_final(t/increment, count, coord)
 				locations.append(position)
 			wrapper.append(locations)
-			count += 1
+			count += 1 # indicates index of particle in self.particles array
 		return wrapper
 
 
 	# wrapper function for particle interactions
 	def simulate(self, t):
-		x_pos = self.generate_pos(t, 0)
-		y_pos = self.generate_pos(t, 1)
+		x_pos = self.generate_pos(t, 0) # 0 indicates X in X,Y pair
+		y_pos = self.generate_pos(t, 1) # 1 indicates Y in X,Y pair
 
 		self.display(x_pos, y_pos)
 
